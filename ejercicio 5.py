412a9568e6a49f5dd0ec76fcb638fc4aceb60e5b
lauralardies <lrodrlar@myuax.com>
2022-02-17 11:14:32 +0100

    pseudocodigo ej 1

---
+++ ejercicio 5.py
@@ -0,0 +1,21 @@
+# ADIF
+# Descuento de la Warner. Este descuento es del 10 % para 2 niños, 15 % para 3 niños y 18 % para 4 niños. A partir de 5 niños, 
+# el descuento es del 18 %, pero aumenta un 1 % por cada niño por encima de 4.
+
+# Nuestro algoritmo calcula el porcentaje de descuento para cualquier familia, inependientemente del número de niños que tenga.
+
+Algoritmo descuento
+
+Entrada
+
+Resultado 
+
+Precondicion
+
+Variables
+
+Realizacion
+
+Poscondicion
+
+fin descuento
