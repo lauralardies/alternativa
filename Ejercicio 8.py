214ccbe7a8aca89d131e593edc01187351fdd97f
crltsnch <91721777+crltsnch@users.noreply.github.com>
2022-02-18 17:53:38 +0100

    Ejercicio 4

---
+++ Ejercicio 8.py
@@ -0,0 +1,6 @@
+Algoritmo prima_anual
+
+ENTRADA
+    accidentes : ENTERO # Número de accidentes
+     distancia : ENTERO # Distancia recorrida
+   antigüedad : ENTERO # Antigüedad
---
+++ ejercicio 4.py
@@ -0,0 +1,22 @@
+Algoritmo NOTA FINAL ALUMNO
+    VARIABLES
+    Promedio = prom
+    Primer examen = parc1
+    Segundo examen = parc2
+    Tercer examen = parc3
+    Cuarto examen = parc4
+    Nota final = NF
+    Definir prom, parc1, parc2, parc3, parc4, NF Como Real
+
+    ENTRADA
+    Escribir "Ingrese la nota del examen: "
+    Leer parc1
+    Escribir "Ingrese la segunda nota del examen: "
+    Leer parc2
+    Escribir "Ingrese la tercera nota del examen: "
+    Leer parc3
+    Escribir "Ingrese la cuarta nota del examen: "
+    Leer parc4
+
+    PROCESOS
+    Prom <- (parc1+parc2+parc3+parc4)/4
